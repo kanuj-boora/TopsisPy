2075651c1d90174366393fa19c5c2515867184a9
kanuj-boora <boorakanuj@gmail.com>
2023-01-23 00:43:22 +0530

    Updated setup.py file and folder structure

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'Topsis-Kanuj-102017188',         # How you named your package folder (MyLib)
-  packages = ['Topsis-Kanuj-102017188'],   # Chose the same as "name"
-  package_dir = {'':'src'},
+  name = 'topsis-17188',         # How you named your package folder (MyLib)
+  packages = ['topsis-17188'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python Package implementing TOPSIS method for multi-criteria decision making method',   # Give a short description about your library
--- src/topsispy/__init__.py
+++
@@ -1,1 +0,0 @@
-from topsispy import topsis
